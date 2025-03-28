5cc408696452a49013670ef05da9fdf6ec6898cb
murilo nakaguma <murilo.nakaguma@aluno.senai.br>
2025-03-28 08:29:55 -0300

    exercicio 1

--- main.go
+++ main.go
@@ -3,17 +3,15 @@ package main
 import "fmt"
 
 func main() {
-	a, b := 10, 3
-	fmt.Println("a soma é:", a + b)
-	fmt.Println("a subtraçao é:", a - b)
-	fmt.Println("a multiplicaçao é:", a * b)
-	fmt.Println("a divisao é:", a / b)
-	fmt.Println("o resto da divisao:", a % b)
-	
-	a++
-	fmt.Println("incrementar a", a)
-
-	if a > 0 && b > 0 {
-		fmt.Println("numeros positivos")
-	}
+	var numero1 int
+	var numero2 int
+	fmt.Println("digite um numero:")
+	fmt.Scan(&numero1)
+	fmt.Println("digite um numero:")
+	fmt.Scan(&numero2)
+	fmt.Println("a soma dos numeros é:", numero1 + numero2)
+	fmt.Println("a subtraçao do numeros é:", numero1 - numero2)
+	fmt.Println("a multiplicaçao do numero é:", numero1 * numero2)
+	fmt.Println("a divisao do numero é:", numero1 / numero2)
+	fmt.Println("o resto da divisao é:", numero1 % numero2)
 }
