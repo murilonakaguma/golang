44b9c73ef041e18d7779a03b239cfad792f99514
murilo nakaguma <murilo.nakaguma@aluno.senai.br>
2025-03-28 08:44:31 -0300

    exercicio senha

--- main.go
+++ main.go
@@ -3,15 +3,14 @@ package main
 import "fmt"
 
 func main() {
-	var numero1 int
-	var numero2 int
-	fmt.Println("digite um numero:")
-	fmt.Scan(&numero1)
-	fmt.Println("digite um numero:")
-	fmt.Scan(&numero2)
-	fmt.Println("a soma dos numeros é:", numero1 + numero2)
-	fmt.Println("a subtraçao do numeros é:", numero1 - numero2)
-	fmt.Println("a multiplicaçao do numero é:", numero1 * numero2)
-	fmt.Println("a divisao do numero é:", numero1 / numero2)
-	fmt.Println("o resto da divisao é:", numero1 % numero2)
+	var usuario string
+	var senha string
+	fmt.Println("digite um nome de usuario e uma senha")
+	fmt.Scan(&usuario)
+	fmt.Scan(&senha)
+	if usuario == "admin" && senha == "1234"{
+		fmt.Println("usuario correto")
+	}else {
+		fmt.Println("usuario incorreto")
+	}
 }
