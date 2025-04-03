41ae20de4b936e5a0a171cc890a415f7d6c7995b
murilo nakaguma <murilo.nakaguma@aluno.senai.br>
2025-04-03 09:18:37 -0300

    exercicos vetor

--- main.go
+++ main.go
@@ -9,4 +9,21 @@ func main() {
 	fmt.Println(nomes)
 	nomes[2] = "gobetti"
 	fmt.Println(nomes)
+	// Slice 
+	var Score = []int{100, 200, 300,400}
+	fmt.Println(Score)
+	Score[1] = 2
+	fmt.Println(Score, len(Score), cap(Score))
+	rangeOne := Score[1:3]
+	fmt.Println(rangeOne)
+	rangeTwo := Score[2:]
+	fmt.Println(rangeTwo)
+	rangeThree := Score[:3]
+	fmt.Println(rangeThree)
+	var superherois = []string{"robin","flash","iron man"}
+	fmt.Println(superherois)
+	superherois = append(superherois, "ben 10", "batman")
+	fmt.Println(superherois, len(superherois), cap(superherois))
+	superherois[4] = "robin"
+
 }
