d1c90b9e0649b3d545f64206b80bccfda1951010
murilo nakaguma <murilo.nakaguma@aluno.senai.br>
2025-04-10 10:06:05 -0300

    exercico idade

--- main.go
+++ main.go
@@ -6,30 +6,16 @@ import (
 )
 
 func main() {
-	age := 16
-	fmt.Println(age <= 20)
-	fmt.Println(age >= 20)
-	fmt.Println(age == 20)
-	fmt.Println(age != 20)
-	if age < 20 {
-		fmt.Println("menor que 20 anos")
-	} else if age < 10 {
-		fmt.Println("menor que 10 anos")
-	} else {
-		fmt.Println("nao é menor que 20 anos")
+	var idade int
+	fmt.Println("qual sua idade?")
+	fmt.Scan(&idade)
+	if idade <18 {
+		fmt.Println("voce é menor de idade")
+	}else if idade > 18 && idade <= 60{
+		fmt.Println("voce é adulto")
+	}else {
+		fmt.Println("voce é idoso")
 	}
-	names := []string{"manu", "lucas", "bruce","murilo","robin","igor"}
-	for index, value := range names {
-		if index == 1 {
-			fmt.Println("continue apos a posiçao", index, "e valor", value)
-			continue
-		}
-		if index > 2 {
-			fmt.Println("sair apos", index)
-			break
-		}
-		fmt.Println("valor:", index, value)
-	}
+	
+	
 }
-	
-	
