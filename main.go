5e95351a41bc7c4998bbe613714b451f17f0b798
murilo nakaguma <murilo.nakaguma@aluno.senai.br>
2025-04-10 11:09:08 -0300

    banco

--- main.go
+++ main.go
@@ -14,21 +14,26 @@ func main() {
 	var subtraçao float32
 	fmt.Println("insira o valor inicial")
 	fmt.Scan(&saldo)
-	fmt.Println("voce deseja sacar(1) ou depositar(2)?")
+	fmt.Println("voce deseja sacar(1) ou depositar(2) ou encerrar a sessao(3)?")
 	fmt.Scan(&banco)
-	if banco == 1 {
-		fmt.Println("voce deseja sacar que quantia?")
+	if banco == 2 {
+		fmt.Println("voce deseja depositar que quantia?")
 		fmt.Scan(&saldo2)
 		soma = saldo + saldo2
 		fmt.Println("o seu saldo total é", soma)
 
 	}
-	if banco == 2 {
-		fmt.Println("voce deseja depositar que quantia?")
+	if banco == 1 {
+		fmt.Println("voce deseja sacar que quantia?")
 		fmt.Scan(&saldo3)
 		subtraçao = saldo - saldo3
 		fmt.Println("o seu saldo total é", subtraçao)
 	}
-
+	if banco == 3 {
+		fmt.Println("sessao encerrada")
+	}
 	
+
+
+
 }
