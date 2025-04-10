71721128b607b94405e033d79ab51992fdde35fe
murilo nakaguma <murilo.nakaguma@aluno.senai.br>
2025-04-10 08:23:52 -0300

    exercicio vetor

--- main.go
+++ main.go
@@ -2,24 +2,20 @@ package main
 
 import (
 	"fmt"
-	"strings"
-	"sort"
+	
 )
 
 func main() {
-	greeting := "ola mundo"
-	fmt.Println(strings.Contains(greeting, "mundo"))
-	fmt.Println(strings.ReplaceAll(greeting, "ola", "oi"))
-	fmt.Println(strings.ToUpper(greeting))
-	fmt.Println(strings.Index(greeting, "mundo"))
-	fmt.Println(strings.Split(greeting, "mundo"))
-	ages := []int {50, 80, 10}
-	sort.Ints(ages)
-	fmt.Println(ages)
-	index := sort.SearchInts(ages, 50)
-	fmt.Println(index)
-	names := []string{"murilo","maicon","fabiano"}
-	sort.Strings(names)
-	fmt.Println(names)
-	fmt.Println(sort.SearchStrings(names, "murilo"))
+	var numero [5]int
+	var soma int
+	for i := 0; i <5;i++ {
+	fmt.Println("digite cinco numeros:" )
+	fmt.Scan(&numero[i])
+	soma += numero[i]
 }
+
+
+fmt.Println("A soma dos números é:", soma)
+}
+	
+	
