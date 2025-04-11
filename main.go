2064793de3168adca7cae0a51e5a6798984db095
murilo nakaguma <murilo.nakaguma@aluno.senai.br>
2025-04-11 13:26:16 -0300

    funcoes

--- main.go
+++ main.go
@@ -4,36 +4,15 @@ import (
 	"fmt"
 	
 )
-
-func main() {
-	var saldo float32
-	var saldo2 float32
-	var banco int
-	var soma float32
-	var saldo3 float32
-	var subtraçao float32
-	fmt.Println("insira o valor inicial")
-	fmt.Scan(&saldo)
-	fmt.Println("voce deseja sacar(1) ou depositar(2) ou encerrar a sessao(3)?")
-	fmt.Scan(&banco)
-	if banco == 2 {
-		fmt.Println("voce deseja depositar que quantia?")
-		fmt.Scan(&saldo2)
-		soma = saldo + saldo2
-		fmt.Println("o seu saldo total é", soma)
-
-	}
-	if banco == 1 {
-		fmt.Println("voce deseja sacar que quantia?")
-		fmt.Scan(&saldo3)
-		subtraçao = saldo - saldo3
-		fmt.Println("o seu saldo total é", subtraçao)
-	}
-	if banco == 3 {
-		fmt.Println("sessao encerrada")
-	}
-	
-
-
-
+func sayGreeting(nome string){
+	fmt.Println("olá!", nome)
 }
+func addNumber(numero1 int, numero2 int) int{
+	return numero1 + numero2
+}
+func main() {
+	sayGreeting("juvelino")
+	resultado := addNumber(10,20)
+	fmt.Println(resultado)
+
+	}
